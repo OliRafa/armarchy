f5bbbc83d1b331eaff68b10356bb3607e3aa5164
Jon Kinney <jonkinney@gmail.com>
2025-08-29 16:03:27 -0500

    Add asahi-alarm/widevine package to support DRM content in chromium
    This is necessary for spotify to work in the browser since there isn't
    an arm64 compatible desktop version of spotify at the momemtn.

--- install/packages.sh
+++ install/packages.sh
@@ -136,6 +136,7 @@ $PKG_MANAGER -Syy --noconfirm --needed \
 
 if [ -z "$OMARCHY_ARM" ]; then
   $PKG_MANAGER -S --noconfirm --needed \
+    asahi-alarm/widevine \
     1password-beta \
     1password-cli \
     localsend \
